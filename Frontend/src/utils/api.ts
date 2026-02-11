9d677bc8fb7bb68bf4c16225c3ff6549e3c6c768
Janith Ranasinghe <jranasinghe19@gmail.com>
2026-02-11 14:19:46 +0530

    Frontend and backend updates to support deployements

--- Frontend/src/utils/api.ts
+++ Frontend/src/utils/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.PROD
+  ? import.meta.env.VITE_API_BASE_URL
+  : "/api";
+
 const api = axios.create({
-  baseURL: "/api",
+  baseURL,
   withCredentials: true,
 });
 
